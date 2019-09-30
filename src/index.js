4418aba02ed864a497a70c4c985057046c69286d
Vitali Moiseenok <moiseenokv@gmail.com>
2019-09-30 19:29:42 +0300

    feat: add expressionCalculator function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ function eval() {
     return;
 }
 
+function calc(fn) {
+  return new Function('return ' + fn)();
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+  var str = expr.trim().split('').filter(el=>el!="");
+  var devisionByZero = expr.trim().includes('/0') || expr.trim().includes('/ 0');
+  var scopesSeq = [0,0];
+  str.filter(function (el) {
+      if(el == '('){
+          scopesSeq[0] +=1;
+      }else if(el == ')'){
+        scopesSeq[1] +=1;
+      }
+  });
+
+  if(scopesSeq[0]!=scopesSeq[1]){
+    throw new Error('ExpressionError: Brackets must be paired');
+  }
+
+  if(devisionByZero==true){
+    throw new Error('TypeError: Division by zero.');
+  }
+
+  return calc(expr);
 }
 
 module.exports = {
